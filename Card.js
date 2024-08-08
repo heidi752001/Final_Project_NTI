457f202f75c5c790553d14b54d09d9b9bcb726ef
haidy <haidyyasser@gmail.com>
2024-08-08 13:57:37 +0300

    Final_Project_NTI

---
+++ Card.js
@@ -0,0 +1,78 @@
+function myFunction1(){
+    document.getElementById("span1").style.display = "block"; 
+    document.getElementById("span1").style.fontSize = "18px";
+    document.getElementById("span1").style.color = "black";
+    document.getElementById("buy1").style.marginBottom = "0px";
+    document.getElementById("buy1").style.marginTop = "-200px";
+    document.getElementById("buy1").style.marginLeft = "170px";
+}
+function  myFunction2(){
+    document.getElementById("span1").style.display = "none";
+    document.getElementById("buy1").style.marginBottom = "0px";
+    document.getElementById("buy1").style.marginTop = "-40px";
+}
+function myFunction3(){
+    document.getElementById("span2").style.display = "block"; 
+    document.getElementById("span2").style.fontSize = "18px";
+    document.getElementById("span2").style.color = "black";
+    document.getElementById("buy2").style.marginBottom = "0px";
+    document.getElementById("buy2").style.marginTop = "-150px";
+    document.getElementById("buy2").style.marginLeft = "170px";
+}
+function  myFunction4(){
+    document.getElementById("span2").style.display = "none";
+    document.getElementById("buy2").style.marginBottom = "0px";
+    document.getElementById("buy2").style.marginTop = "-40px";
+}
+function myFunction5(){
+    document.getElementById("span3").style.display = "block"; 
+    document.getElementById("span3").style.fontSize = "18px";
+    document.getElementById("span3").style.color = "black";
+    document.getElementById("buy3").style.marginBottom = "0px";
+    document.getElementById("buy3").style.marginTop = "-150px";
+    document.getElementById("buy3").style.marginLeft = "170px";
+}
+function  myFunction6(){
+    document.getElementById("span3").style.display = "none";
+    document.getElementById("buy3").style.marginBottom = "0px";
+    document.getElementById("buy3").style.marginTop = "-40px";
+}
+function myFunction7(){
+    document.getElementById("span4").style.display = "block"; 
+    document.getElementById("span4").style.fontSize = "18px";
+    document.getElementById("span4").style.color = "black";
+    document.getElementById("buy4").style.marginBottom = "0px";
+    document.getElementById("buy4").style.marginTop = "-200px";
+    document.getElementById("buy4").style.marginLeft = "170px";
+}
+function  myFunction8(){
+    document.getElementById("span4").style.display = "none";
+    document.getElementById("buy4").style.marginBottom = "0px";
+    document.getElementById("buy4").style.marginTop = "-40px";
+}
+function myFunction9(){
+    document.getElementById("span5").style.display = "block"; 
+    document.getElementById("span5").style.fontSize = "18px";
+    document.getElementById("span5").style.color = "black";
+    document.getElementById("buy5").style.marginBottom = "0px";
+    document.getElementById("buy5").style.marginTop = "-150px";
+    document.getElementById("buy5").style.marginLeft = "170px";
+}
+function  myFunction10(){
+    document.getElementById("span5").style.display = "none";
+    document.getElementById("buy5").style.marginBottom = "0px";
+    document.getElementById("buy5").style.marginTop = "-40px";
+}
+function myFunction11(){
+    document.getElementById("span6").style.display = "block"; 
+    document.getElementById("span6").style.fontSize = "18px";
+    document.getElementById("span6").style.color = "black";
+    document.getElementById("buy6").style.marginBottom = "0px";
+    document.getElementById("buy6").style.marginTop = "-150px";
+    document.getElementById("buy6").style.marginLeft = "170px";
+}
+function  myFunction12(){
+    document.getElementById("span6").style.display = "none";
+    document.getElementById("buy6").style.marginBottom = "0px";
+    document.getElementById("buy6").style.marginTop = "-40px";
+}
